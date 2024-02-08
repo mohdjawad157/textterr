72f0dc2521867b555532249b8ab61d874f210945
mohdjawad157 <mohdjawad157@gmail.com>
2024-02-08 19:42:19 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,44 +1,72 @@
+import "./App.css";
 
-import './App.css';
+import Navbar from "./components/Navbar";
+import TextForm from "./components/Textform";
+import About from "./components/About";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState(`light`); // Whether dark mode is enable or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+
+  // Toggle  Dark Mode
+  const toggleMode = () => {
+    if (mode === `light`) {
+      setMode(`dark`);
+      document.body.style.backgroundColor = "#020334";
+      showAlert(`Dark mode has been enabled`, "success");
+      document.title = "TextUtils - Dark Mode";
+    } else {
+      setMode(`light`);
+      document.body.style.backgroundColor = "white";
+      showAlert(`Light mode has been enabled`, "success");
+      document.title = "TextUtils - Light Mode";
+    }
+  };
+
   return (
     <>
-   <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">TextUtils</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">About</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="/">Action</a></li>
-            <li><a className="dropdown-item" href="/">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="/">Something else here</a></li>
-          </ul>
-        </li>
-       
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+      <BrowserRouter>
+        <Navbar
+          title="TextUtils"
+          aboutText="About Us"
+          mode={mode}
+          toggleMode={toggleMode}
+        />
+
+        <Alert alert={alert} />
+        <div className="container my-3" mode={mode}>
+          <Routes>
+            <Route path="/about" element={<About />} />
+          </Routes>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <TextForm
+                  showAlert={showAlert}
+                  heading="Enter the text to analyze below"
+                  mode={mode}
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
